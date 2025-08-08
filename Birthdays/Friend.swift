4db4339ab370db217ea0b61d9294c58be4792800
joan <nustellapham@gmail.com>
2025-08-08 11:24:05 -0400

    created navigation to add new friends and birthdays.

--- Birthdays/Friend.swift
+++ Birthdays/Friend.swift
@@ -6,13 +6,15 @@
 //
 
 import Foundation
+import SwiftData
 
 //adding name and birthday//
 class Friend {
     var name: String
     var birthday: Date
     
-    init (name: String, birthday: Date)
-    self.name = name
-    self.birthday = birthday
+    init (name: String, birthday: Date) {
+        self.name = name
+        self.birthday = birthday
+    }
 }
